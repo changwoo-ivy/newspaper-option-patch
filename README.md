91d7c95a98a4d9bc1cfd8a030605ee5b86876909
changwoo <ep6tri@hotmail.com>
2017-08-24 12:45:34 +0900

    플러그인 동작 코드 커밋.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# newspaper-option-patch
-오래된 NewsPaper 테마의 보안 이슈에 대응하는 플러그인입니다.
+# newspaper-theme-patch
+스페이퍼 보안 이슈 대응 (2017. 08. 24.) 패치 플러그인입니다.
---
+++ newspaper-theme-patch.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Plugin Name: Newspaper Theme Patch
+ * Description: 뉴스페이퍼 보안 이슈 대응 (2017. 08. 24.) 패치 플러그인입니다.
+ * Author:      남창우
+ * Version:     1.0.0
+ * Author URI:  mailto://changwoo@ivynet.co.kr
+ * Plugin URI:  https://github.com/changwoo-ivy/newspaper-theme-patch
+ */
+
+add_action( 'init', 'ntp_theme_patch' );
+
+function ntp_theme_patch() {
+
+	/** @var WP_Theme $my_theme */
+	$my_theme = wp_get_theme();
+
+	/** @var NULL|WP_Theme $parnet */
+	$parent = $my_theme->parent();
+
+	if ( 'Newsmag' === $my_theme->get( 'Name' ) || ( $parent && 'Newsmag' === $parent->get( 'Name' ) ) ) {
+		do_action( 'ntp_patches' );
+	}
+}
+
+add_action( 'ntp_patches', 'ntp_fix_update_option' );
+
+function ntp_fix_update_option() {
+	add_filter( 'pre_update_option_td_010', 'ntp_patch_option_td_010', 1 );
+}
+
+
+function ntp_option_requires_capabilities( $value ) {
+	if ( ! current_user_can( 'manage_options' ) ) {
+		wp_die( 'You are not authorized.', 'Unauthorized' );
+	}
+
+	return $value;
+}
